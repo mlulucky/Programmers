cb2d31928e63b3c6941168ceae471cd4e51b0497
mlulucky <dmswjdans@gmail.com>
2023-07-30 14:01:29 +0900

    특수문자 출력하기

---
+++ src/basic_training/Day01_5.java
@@ -0,0 +1,16 @@
+package basic_training;
+
+public class Day01_5 {
+    public static void main(String[] args) {
+        // 자바 특수문자
+        // 탭 : \t
+        // 줄바꿈 : \n
+        // 작은 따옴표(') : \'
+        // 큰 따옴표(") : \"
+        // 역슬래시(\) : \\
+        // 나머지 특수문자는 그대로 출력
+        String str = "!@#$%^&*(\\\'\"<>?:;";
+        System.out.println(str);
+
+    }
+}
