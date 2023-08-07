e2813a6c9d55b8180e44cb2dba2ce5f8b69fb4d4
mlulucky <dmswjdans@gmail.com>
2023-08-07 22:17:33 +0900

    공배수

--- src/basic_training/Day04_2.java
+++ src/basic_training/Day04_2.java
@@ -1,32 +1,12 @@
 package basic_training;
 
-<<<<<<< HEAD
 public class Day04_2 {
-    public int solution(int number, int n, int m) {
-=======
-import java.util.Scanner;
 
-public class Day04_2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("number 를 입력해주세요");
-        int number = sc.nextInt();
-        System.out.print("n 를 입력해주세요");
-        int n = sc.nextInt();
-        System.out.print("m 를 입력해주세요");
-        int m = sc.nextInt();
-        solution(number, n, m);
-    }
-    public static int solution(int number, int n, int m) {
->>>>>>> a19feb4 (공배수)
-        int answer = 0;
-        if(number % n == 0 && number % m == 0) {
-            answer = 1;
-        }
-<<<<<<< HEAD
-=======
-       System.out.println("answer " + answer);
->>>>>>> a19feb4 (공배수)
-        return answer;
-    }
+	public static int solution(int number, int n, int m) {
+		int answer = 0;
+		if (number % n == 0 && number % m == 0) {
+			answer = 1;
+		}
+		return answer;
+	}
 }
