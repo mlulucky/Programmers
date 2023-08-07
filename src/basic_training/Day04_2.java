b4fbcdf266918ee63f55944a774cd49b390b6983
mlulucky <dmswjdans@gmail.com>
2023-08-07 22:17:33 +0900

    공배수

---
+++ src/basic_training/Day04_2.java
@@ -0,0 +1,11 @@
+package basic_training;
+
+public class Day04_2 {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        if(number % n == 0 && number % m == 0) {
+            answer = 1;
+        }
+        return answer;
+    }
+}
