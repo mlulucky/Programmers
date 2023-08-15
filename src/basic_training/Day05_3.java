b276dee938303875cf4b5a0024c5721bf7f9aaa1
mlulucky <dmswjdans@gmail.com>
2023-08-15 23:09:23 +0900

    주사위 게임 2

---
+++ src/basic_training/Day05_3.java
@@ -0,0 +1,19 @@
+package basic_training;
+
+public class Day05_3 {
+    public int solution(int a, int b, int c) {
+        int answer=0;
+
+        if(a!=b && b!=c && a!=c) { 
+            answer = a+b+c;
+        }
+        if(a==b || b==c || a==c) { 
+            answer = (a+b+c) * (a*a + b*b + c*c);
+        }
+        if(a==b && b==c) { 
+            answer = (a+b+c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+        }
+
+        return answer;
+    }
+}
