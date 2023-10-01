ca17d3b2a346f526ddee6bac1fc5c37b79ba8fbc
mlulucky <dmswjdans@gmail.com>
2023-10-01 22:05:14 +0900

    글자 지우기 - 문제풀이 추가

--- src/basic_training/Day11_3.java
+++ src/basic_training/Day11_3.java
@@ -10,6 +10,7 @@ public class Day11_3 {
 		for(int idx : indices) {
 			intArr.add(idx);
 		}
+
 		for(int i=0; i<my_string.length(); i++){
 			if(!intArr.contains(i)) { 
 				sb.append(my_string.charAt(i));
@@ -22,6 +23,15 @@ public class Day11_3 {
 
 // 🍒 자바 List배열 contains() 메서드
 // List 클래스에서 제공하는 contains() 메서드. 배열에 특정 값이 존재하는지 확인하는 법
+
+// 🍉 번외 문제풀이 _ IntStream (시간 오래걸림)
+// StringBuilder sb = new StringBuilder();
+// List<Integer> intArr = new ArrayList<>();
+// for(int idx : indices) {
+// 	intArr.add(idx);
+// }
+// String resultStr = IntStream.range(0, my_string.length()).filter(i -> !intArr.contains(i)).mapToObj(my_string::charAt).map(String::valueOf).collect(Collectors.joining());
+// return sb.append(resultStr).toString();
 
 // 🍉 참고할 만한 번외 문제풀이
 //  StringBuilder sb = new StringBuilder(my_string);
