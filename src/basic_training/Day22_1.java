c4e3b9a9cf77b80eb6215a5fa52608e5b93de490
mlulucky <dmswjdans@gmail.com>
2023-11-30 21:42:25 +0900

    0떼기

---
+++ src/basic_training/Day22_1.java
@@ -0,0 +1,10 @@
+package basic_training;
+
+public class Day22_1 {
+	public String solution(String n_str) {
+		return String.valueOf(Integer.parseInt(n_str));
+	}
+}
+
+// 🤔 참고할만한 번외 문제풀이
+// return ""+Integer.parseInt(n_str);
