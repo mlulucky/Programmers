5f0dda5da354e01463bc0c634ada474283260d2e
mlulucky <dmswjdans@gmail.com>
2024-01-07 15:29:59 +0900

    특별한 이차원 배열2

---
+++ src/basic_training/Day25_2.java
@@ -0,0 +1,12 @@
+package basic_training;
+
+public class Day25_2 {
+	public int solution(int[][] arr) {
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr.length; j++) {
+				if(arr[i][j] != arr[j][i]) return 0;
+			}
+		}
+		return 1;
+	}
+}	
