3896bacf1b3f8df01743e2a2da7813a182749817
mlulucky <dmswjdans@gmail.com>
2024-01-13 18:54:14 +0900

    이차원 배열 대각선 순회하기

---
+++ src/basic_training/Day25_4.java
@@ -0,0 +1,15 @@
+package basic_training;
+
+public class Day25_4 {
+	public int solution(int[][] board, int k) {
+		int sum = 0;
+		for(int i=0; i<board.length; i++) {
+			for(int j=0; j<board[i].length; j++) {
+				if(i+j <= k) {
+					sum += board[i][j];
+				}
+			}
+		}
+		return sum;
+	}
+}
