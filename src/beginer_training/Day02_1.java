bbb1d212a83875b0486a5f3ff610dbc0c9cc6615
mlulucky <dmswjdans@gmail.com>
2024-01-15 21:44:05 +0900

    두 수의 나눗셈

---
+++ src/beginer_training/Day02_1.java
@@ -0,0 +1,10 @@
+package beginer_training;
+
+public class Day02_1 {
+	public int solution(int num1, int num2) {
+		return (int)(((double)num1/num2)*1000);
+	}
+}
+
+// 참고할만한 번외 문제풀이
+// return num1 * 1000 / num2;
