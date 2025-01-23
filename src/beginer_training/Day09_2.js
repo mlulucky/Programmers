a9a567cbb5ac08e4a93908cbde038eb82c67c26f
mlulucky <dmswjdans@gmail.com>
2025-01-23 23:37:14 +0900

    모스 부호(1)

---
+++ src/beginer_training/Day09_2.js
@@ -0,0 +1,14 @@
+function solution(letter) {
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+
+    return letter.split(' ').map((value, idx) => morse[value]).join('');
+}
+
+// 다른방법 : reduce
+// letter.split(' ').reduce((acc, curr) => acc += morse[curr] ,'');
